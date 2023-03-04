a5f70f6a150e19fcf6dc6162178b6eb3a38903de
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-03-04 13:28:41 +0200

    done 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gelleryRef = document.querySelector('.gallery');
+const cardMarkup = createGalleryItems(galleryItems);
 
-console.log(galleryItems);
+gelleryRef.insertAdjacentHTML('beforeend', cardMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item"
+   href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+           alt="${description}"
+    />
+</a>
+`;
+    })
+    .join('');
+}
