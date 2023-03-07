39f82caa2eb4272834e4c3f3f31715f5dadafdaa
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-03-07 16:30:17 +0200

    add main functions in feedbsck form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+const feedbackForm = document.querySelector('.feedback-form');
+const selectedDataForms = {};
 
+populateDataForm();
+
+feedbackForm.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  const formData = new FormData(feedbackForm);
+  formData.forEach((value, name) => {
+    console.log(value, name);
+  });
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+});
+feedbackForm.addEventListener('input', evt => {
+  selectedDataForms[evt.target.name] = evt.target.value;
+  console.log(selectedDataForms);
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(selectedDataForms)
+  );
+});
+
+function populateDataForm() {
+  let persistedDatas = localStorage.getItem('feedback-form-state');
+  if (persistedDatas) {
+    persistedDatas = JSON.parse(persistedDatas);
+    Object.entries(persistedDatas).forEach(([email, message]) => {
+      selectedDataForms[email] = message;
+      feedbackForm.elements[email].value = message;
+    });
+  }
+}
