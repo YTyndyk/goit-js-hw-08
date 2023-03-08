b9f05ea0427e6e0864fa7ef4af10b44f0123769d
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-03-08 11:36:49 +0200

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,37 @@
 import throttle from 'lodash.throttle';
+
 const STORAGE_KEY = 'feedback-form-state';
 const feedbackForm = document.querySelector('.feedback-form');
-const selectedDataForms = {};
 
 populateDataForm();
 
-feedbackForm.addEventListener('submit', evt => {
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onInputData, 500));
+
+function onFormSubmit(evt) {
   evt.preventDefault();
 
   const formData = new FormData(feedbackForm);
   formData.forEach((value, name) => {
     console.log(value, name);
   });
+
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
-});
-feedbackForm.addEventListener('input', evt => {
-  selectedDataForms[evt.target.name] = evt.target.value.trim();
-  console.log(selectedDataForms);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(selectedDataForms));
-});
+}
+
+function onInputData(evt) {
+  let persistedDatas = localStorage.getItem(STORAGE_KEY);
+  persistedDatas = persistedDatas ? JSON.parse(persistedDatas) : {};
+  persistedDatas[evt.target.name] = evt.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedDatas));
+}
 
 function populateDataForm() {
   let persistedDatas = localStorage.getItem(STORAGE_KEY);
   if (persistedDatas) {
     persistedDatas = JSON.parse(persistedDatas);
     Object.entries(persistedDatas).forEach(([email, message]) => {
-      selectedDataForms[email] = message;
       feedbackForm.elements[email].value = message;
     });
   }
