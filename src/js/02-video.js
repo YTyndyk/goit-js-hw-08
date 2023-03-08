ae435e73056092439324def4621589b688c94233
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-03-08 16:23:11 +0200

    task 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)) || 0);
